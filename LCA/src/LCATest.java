5f0fa6d1012dce5428762ed1cf8ad796c145db08
darraghmasterson <dmasters@tcd.ie>
2018-09-28 22:12:47 +0100

    tests

--- LCA/src/LCATest.java
+++ LCA/src/LCATest.java
@@ -1,12 +1,26 @@
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LCATest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		BinaryTree tree = new BinaryTree();
+		tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(5); 
+        tree.root.right.left = new Node(6); 
+        tree.root.right.right = new Node(7); 
+  
+        Assert.assertEquals("lca of 4 and 5", tree.findLCA(4, 5), 2);
+        Assert.assertEquals("LCA(4, 6): " , tree.findLCA(4,6),1); 
+        Assert.assertEquals("LCA(3, 4): ", tree.findLCA(3,4), 1); 
+        Assert.assertEquals("LCA(2, 4): ", tree.findLCA(2,4),2); 
+        
 	}
 
 }
