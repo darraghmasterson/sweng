c8700bf3ec77d232fa16414a99d024e1a07e2854
darraghmasterson <dmasters@tcd.ie>
2018-10-26 12:53:56 +0100

    created DAG class and Test class

---
+++ LCA/src/DAG.java
@@ -0,0 +1,140 @@
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+/**
+ *  The {@code Digraph} class represents a directed graph of vertices
+ *  named 0 through <em>V</em> - 1.
+ *  It supports the following two primary operations: add an edge to the digraph,
+ *  iterate over all of the vertices adjacent from a given vertex.
+ *  Parallel edges and self-loops are permitted.
+ *  <p>
+ *  This implementation uses an adjacency-lists representation, which 
+ *  is a vertex-indexed array of {@link Bag} objects.
+ *  All operations take constant time (in the worst case) except
+ *  iterating over the vertices adjacent from a given vertex, which takes
+ *  time proportional to the number of such vertices.
+ *  <p>
+ *  For additional documentation,
+ *  see <a href="https://algs4.cs.princeton.edu/42digraph">Section 4.2</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+
+public class DAG {
+    private static final String NEWLINE = System.getProperty("line.separator");
+
+    private final int V;           // number of vertices in this digraph
+    private int E;                 // number of edges in this digraph
+    private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
+    private int[] indegree;        // indegree[v] = indegree of vertex v
+    
+    /**
+     * Initializes an empty digraph with <em>V</em> vertices.
+     *
+     * @param  V the number of vertices
+     * @throws IllegalArgumentException if {@code V < 0}
+     */
+    public DAG(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        indegree = new int[V];
+        adj = (ArrayList<Integer>[]) new ArrayList[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new ArrayList<Integer>();
+        }
+    }
+
+  
+        
+    /**
+     * Returns the number of vertices in this digraph.
+     *
+     * @return the number of vertices in this digraph
+     */
+    public int V() {
+        return V;
+    }
+
+    /**
+     * Returns the number of edges in this digraph.
+     *
+     * @return the number of edges in this digraph
+     */
+    public int E() {
+        return E;
+    }
+
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    /**
+     * Adds the directed edge vw to this digraph.
+     *
+     * @param  v the tail vertex
+     * @param  w the head vertex
+     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+     */
+    public void addEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(w);
+        indegree[w]++;
+        E++;
+    }
+
+    /**
+     * Returns the vertices adjacent from vertex {@code v} in this digraph.
+     *
+     * @param  v the vertex
+     * @return the vertices adjacent from vertex {@code v} in this digraph, as an iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<Integer> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    /**
+     * Returns the number of directed edges incident from vertex {@code v}.
+     * This is known as the <em>outdegree</em> of vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the outdegree of vertex {@code v}               
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int outdegree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+    /**
+     * Returns the number of directed edges incident to vertex {@code v}.
+     * This is known as the <em>indegree</em> of vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the indegree of vertex {@code v}               
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int indegree(int v) {
+        validateVertex(v);
+        return indegree[v];
+    }
+
+
+    
+
+    /**
+     * Unit tests the {@code Digraph} data type.
+     *
+     * @param args the command-line arguments
+     */
+
+}
--- LCA/src/LCATest.java
+++ LCA/src/LCATest.java
@@ -77,9 +77,4 @@ public class LCATest {
 		
 	}
 
-	@Test
-	public void testDAR()
-	{
-		
-	}
 }
